70902c18b156e7199aeb73642ab6cd6eaaeed636
Dusan Sacha <dusan.sacha@cadaxo.com>
2018-02-12 15:19:39 +0000

    Workshop and footer update

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,6 +4,17 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("cadaxo.appobject-page-dusan.controller.App", {
-
+		gotoLinkedin: function(oEvent) {
+			window.open("https://www.linkedin.com/in/dusansacha/"); // eslint-disable-line sap-no-hardcoded-url
+		},
+		gotoCadaxo: function(oEvent) {
+			window.open("http://www.cadaxo.com");	// eslint-disable-line sap-no-hardcoded-url
+		},
+		gotoTwitter: function(oEvent) {
+			window.open("https://twitter.com/sacha_dusan");	// eslint-disable-line sap-no-hardcoded-url
+		},
+		gotoGithub: function(oEvent) {
+			window.open("https://github.com/cadaxo/object-page-dusan");	// eslint-disable-line sap-no-hardcoded-url
+		}			
 	});
 });
